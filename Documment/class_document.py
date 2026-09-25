80113aa970e3a814497b73d6a1e1b8b8ec46f528
Yanis <yanis.thollet69@gmail.com>
2026-09-25 16:17:48 +0200

    4.2 ok

--- Documment/class_document.py
+++ Documment/class_document.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 @dataclass
 class Document:
-    
+
     titre: str
     """ Titre du document """
 
@@ -19,3 +19,29 @@ class Document:
 
     texte: str
     """ Contenu textuel du document """
+
+    def infos(self):
+
+        for k,v in vars(self).items():
+            print(f'{k} : {v}')
+
+
+    def __str__(self):
+
+        return f'Titre : {self.titre}'
+
+
+
+if __name__ == '__main__':
+
+    document = Document(
+        auteur = 'Jean michel',
+        date = datetime.now(),
+        texte = 'blablabla',
+        titre = 'titre1',
+        url = 'http://ckhnvubvfzubaefifabù'
+    )
+    document.infos()
+
+    print()
+    print(document)
